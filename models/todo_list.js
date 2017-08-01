115c19037af9a609e070e7fdd6b521eb017f532c
Rémy <git@rrelmy.ch>
2017-08-01 23:39:06 +0200

    reverse user todo relation

--- models/todo_list.js
+++ models/todo_list.js
@@ -1,11 +1,13 @@
 const Sequelize = require('sequelize')
 const sequelize = require('../support/db')
 const TodoItem = require('./todo_item')
+const User = require('./user')
 
 const TodoList = sequelize.define('todo_list', {
     title: {type: Sequelize.STRING, allowNull: false}
 })
 
 TodoList.Items = TodoList.hasMany(TodoItem, {foreignKey: {allowNull: false}})
+TodoList.belongsTo(User)
 
 module.exports = TodoList
