1e72167500c85883cbf2f2e41b411667b16aff40
Rémy <git@rrelmy.ch>
2017-08-01 22:36:51 +0200

    improve user resolver

--- schema/query/user.js
+++ schema/query/user.js
@@ -1,19 +1,13 @@
-const User = require('../../models/user')
-const {resolver} = require('graphql-sequelize')
 const userType = require('../type/user')
 
 module.exports = {
     description: 'Get current user',
     type: userType,
-    resolve: resolver(User, {
-        before: function(findOptions, args, context, info) {
-            if (!context.user || !context.user.id) {
-                throw new Error("No current user!");
-            }
-            findOptions.where = {
-                id: context.user.id
-            };
-            return findOptions;
+    resolve: function (root, args, context) {
+        if (!context.user || !context.user.id) {
+            throw new Error("No current user!");
         }
-    })
+
+        return context.user
+    }
 }
